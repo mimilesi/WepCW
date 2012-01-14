12d742ef4689202ff6e54c64abb445b422c62250
Marianna Milesi <mm00128@surrey.ac.uk>
2012-01-14 22:14:05 +0000

    Adding friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
        render 'new'    
      else
        sign_in user
-       redirect_to user
+       redirect_back_or user
     end
   end
 
@@ -20,5 +20,9 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end  
+  
+  def authenticate
+    deny_access unless signed_in?
+  end
   
 end
